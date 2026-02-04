dc65152a7c807daa2b2ce811c1ba4c54d3419a9a
SuperFranky <franklin.power21@gmail.com>
2026-02-04 08:46:36 +0100

    project clean up and restructure

--- yellow-agent/debug-nitrolite.ts
+++
@@ -1,9 +0,0 @@
-
-import * as Nitrolite from '@erc7824/nitrolite';
-
-console.log('Keys exported by Nitrolite:', Object.keys(Nitrolite));
-try {
-  console.log('parseRPCResponse:', Nitrolite.parseRPCResponse);
-} catch (e) {
-  console.log('parseRPCResponse not found');
-}
